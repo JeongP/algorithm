1feca1b27f6074631109498b26facad90f9c962a
JeongPyo <wjdvy0409@naver.com>
2020-03-07 20:09:08 +0900

    13913

---
+++ boj_lec/sw_practice/part2_BFS/13913.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+const int MAX = 100000+1;
+int N,K;
+queue<pair<int,int>> q;
+// 본인의 이전 루틴 idx를 담을 배열
+int parent[MAX];
+bool visit[MAX];
+// 경로 담을 배열.
+vector<int> path;
+
+
+void input() {
+    cin >> N >> K;
+}
+
+void prt(int n) {
+    cout << n << endl;
+    
+    // 지나온 경로들 벡터에 넣어주기.
+    int idx=K;
+    while(idx!=N) {
+        path.push_back(idx);
+        idx = parent[idx];
+    }
+    path.push_back(N);
+    
+    for(int i=path.size()-1;i>=0;i--) {
+        cout << path[i] << " ";
+    }
+}
+
+
+void solution () {
+    q.push(make_pair(N,0));
+    while(!q.empty()) {
+        
+        for(int i=0;i<q.size();i++) {
+            int cur_num= q.front().first;
+            int cnt = q.front().second;
+            q.pop();
+            
+            // 목적지에 도달 했을 때.
+            if(cur_num == K) {      
+                prt(cnt);
+                return;
+            }
+
+            if(!visit[cur_num -1] && cur_num - 1 >= 0) {
+                visit[cur_num -1] = true;
+                parent[cur_num-1] = cur_num;
+                q.push(make_pair(cur_num-1,cnt+1));
+            }
+            if(!visit[cur_num +1] && cur_num+1<=MAX) {
+                visit[cur_num +1] = true;
+                parent[cur_num+1] = cur_num;
+                q.push(make_pair(cur_num+1,cnt+1));
+            }
+            if(!visit[cur_num*2] && cur_num*2<=MAX) {
+                visit[cur_num*2] = true;
+                parent[cur_num*2] = cur_num;
+                q.push(make_pair(cur_num*2,cnt+1));
+            }
+            
+        }
+    }
+}
+int main () {
+    input();
+    solution();
+}
