962c395e0d0f9d6ea890ab2e49bb700f1944454c
JeongPyo <wjdvy0409@naver.com>
2020-03-14 23:47:20 +0900

    1152.cpp

---
+++ boj_step/ch8_string/1152.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+
+
+int main () {
+    string s;
+    int cnt = 0;
+    bool space = false;
+    getline(cin, s);
+    
+    for(int i=0;i<s.length();i++) {
+        if(s[i] == ' ') {
+            space = true;
+        } else if (space) {
+            space = false;
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+    return 0;
+}
